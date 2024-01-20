7a3a2d4283fe1daa745e859f7993f4792324612e
wjhers <wujh2117@mails.jlu.edu.cn>
2024-01-20 23:37:34 +0800

    update: 中文

--- ui/src/main.js
+++ ui/src/main.js
@@ -8,10 +8,14 @@ import store from './store'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 
+// 中文
+import locale from 'element-plus/es/locale/lang/zh-cn'
+
+
 const app = createApp(App)
 
 app.use(router)
 app.use(store)
-app.use(ElementPlus)
+app.use(ElementPlus, {locale})
 
 app.mount('#app')
